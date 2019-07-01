3f13d2a33e443daee8ab76914215ba23e1298942
Bhagya-laxmi <50071928+Bhagya-laxmi@users.noreply.github.com>
2019-07-01 08:58:21 +0200

    gui and db

---
+++ Laptop_Python/Db&gui/gui_func1.py
@@ -0,0 +1,63 @@
+import time
+import sqlite3
+from tkinter import *
+import matplotlib.pyplot as plt
+import matplotlib
+import datetime
+
+conn = sqlite3.connect('Database_sensor.db')
+Lt= conn.cursor()
+
+def plot(*args):
+    if selectedPlot.get() == 'Temperature_Plot':
+        Lt.execute("SELECT * FROM Data WHERE Topic = 'SmartCities/Temperature'")
+        plt.figure()
+    elif selectedPlot.get() == 'Humididty_Plot':
+        Lt.execute("SELECT * FROM Data WHERE Topic = 'SmartCities/Humidity' ")
+        plt.figure()
+    elif selectedPlot.get() == 'Motion_Plot':
+        Lt.execute("SELECT * FROM Data WHERE Topic = 'SmartCities/Motion' ")
+        plt.figure()
+    elif selectedPlot.get() == 'PeopleCount_Plot':
+        Lt.execute("SELECT * FROM Data WHERE Topic = 'SmartCities/PeopleCount' ")
+        plt.figure()
+    elif selectedPlot.get() == 'Light_Plot':
+        Lt.execute("SELECT * FROM Data WHERE Topic = 'SmartCities/Light' ")
+        plt.figure()
+
+    #WHERE topic = temperature
+    time_plot= []
+    sensor_plot=[]
+
+    for row in Lt.fetchall():
+        converted_dates = matplotlib.dates.datestr2num(row[0])
+        time_plot.append(converted_dates)
+        sensor_plot.append(row[2])              
+    plt.xlabel('time')
+    plt.ylabel('sensor_data')
+    plt.title(selectedPlot.get())
+    plt.grid()
+    plt.plot_date(time_plot,sensor_plot, '-',label= selectedPlot.get())
+    plt.legend(loc ='upper left')
+    plt.show()
+
+
+
+mainwindow = Tk()
+mainwindow.configure(background = 'white')
+mainwindow.geometry('200x100')
+PlotList=["Temperature_Plot", "Humididty_Plot","Motion_Plot","PeopleCount_Plot","Light_Plot"]
+
+selectedPlot = StringVar(mainwindow)
+selectedPlot.set(PlotList[0])
+
+PlotMenu= OptionMenu(mainwindow,selectedPlot,*PlotList)
+PlotMenu.pack()
+
+button = Button(mainwindow, text="plot", command=plot)
+button.pack()
+mainwindow.mainloop()
+
+
+Lt.close()
+conn.close()
--- Laptop_Python/mqtt_test_laptop/mqtt_test_Laptop.py
+++ Laptop_Python/mqtt_test_laptop/mqtt_test_Laptop.py
@@ -1,5 +1,5 @@
 import paho.mqtt.publish as publish
 
-publish.single("SmartCities/fromLaptop", "Hello from Laptop", hostname="test.mosquitto.org")
+publish.single("SmartCities/Temperature", 300, hostname="iot.eclipse.org")
 #publish.single("CoreElectronics/topic", "World!", hostname="test.mosquitto.org")
 print("Done")
